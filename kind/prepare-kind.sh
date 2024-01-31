5b0f9f70ae80bb184a24c90a68c18a9fb569b232
Göran Pöhner <goeran.poehner@gmail.com>
2024-01-31 14:38:23 +0100

    Switched to weave-network to have NetworkPolicy support

--- kind/prepare-kind.sh
+++ kind/prepare-kind.sh
@@ -2,3 +2,4 @@
 echo ">>>>> Bootstrapping Kind"
 kind create cluster --name $1 --config kind-default.yaml --image kindest/node:v1.29.0@sha256:54a50c9354f11ce0aa56a85d2cacb1b950f85eab3fe1caf988826d1f89bf37eb
 echo  "<<<<< Kind ready."
+kubectl apply -f https://github.com/weaveworks/weave/releases/download/v2.8.1/weave-daemonset-k8s.yaml
