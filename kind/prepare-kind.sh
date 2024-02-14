73512be3161b31918aea373b3f115fc076b01e71
Goeran Poehner <goeran.poehner@gmail.com>
2024-02-14 16:35:11 +0100

    Callico with multi control-plane cluster

--- kind/prepare-kind.sh
+++ kind/prepare-kind.sh
@@ -2,4 +2,5 @@
 echo ">>>>> Bootstrapping Kind"
 kind create cluster --name $1 --config kind-default.yaml --image kindest/node:v1.29.0@sha256:54a50c9354f11ce0aa56a85d2cacb1b950f85eab3fe1caf988826d1f89bf37eb
 echo  "<<<<< Kind ready."
-kubectl apply -f https://github.com/weaveworks/weave/releases/download/v2.8.1/weave-daemonset-k8s.yaml
+kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.27.0/manifests/tigera-operator.yaml
+kubectl create -f custom-resources.yaml
