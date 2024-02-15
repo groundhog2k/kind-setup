2d0cc24fc8575603ce969736abcccd1f1661672b
Goeran Poehner <goeran.poehner@gmail.com>
2024-02-15 21:50:12 +0100

    Added Callico CNI for policy support

--- kind/prepare-kind.sh
+++ kind/prepare-kind.sh
@@ -2,5 +2,4 @@
 echo ">>>>> Bootstrapping Kind"
 kind create cluster --name $1 --config kind-default.yaml --image kindest/node:v1.29.0@sha256:54a50c9354f11ce0aa56a85d2cacb1b950f85eab3fe1caf988826d1f89bf37eb
 echo  "<<<<< Kind ready."
-kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.27.0/manifests/tigera-operator.yaml
-kubectl create -f custom-resources.yaml
+kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.27.0/manifests/calico.yaml
