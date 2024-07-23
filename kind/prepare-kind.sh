aa944fb3ed9812ce13f849cf25676a7fd2a7355c
Göran Pöhner <goeran.poehner@gmail.com>
2024-07-23 14:58:53 +0200

    Updated calico

--- kind/prepare-kind.sh
+++ kind/prepare-kind.sh
@@ -2,4 +2,4 @@
 echo ">>>>> Bootstrapping Kind"
 kind create cluster --name $1 --config kind-default.yaml --image kindest/node:v1.29.4@sha256:3abb816a5b1061fb15c6e9e60856ec40d56b7b52bcea5f5f1350bc6e2320b6f8
 echo  "<<<<< Kind ready."
-kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.27.0/manifests/calico.yaml
+kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.27.4/manifests/calico.yaml
