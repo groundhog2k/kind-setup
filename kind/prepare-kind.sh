c04e8333fbeca73cb2f6f5568a3d0b1737c0fdbb
Goeran Poehner <goeran.poehner@gmail.com>
2025-02-15 13:01:17 +0100

    Fixed config export

--- kind/prepare-kind.sh
+++ kind/prepare-kind.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo ">>>>> Bootstrapping Kind"
 kind create cluster --name $1 --config kind-default.yaml --image kindest/node:v1.32.2@sha256:f226345927d7e348497136874b6d207e0b32cc52154ad8323129352923a3142f
-kind export kubeconfig -n devcluster
+kind export kubeconfig --name $1
 echo  "<<<<< Kind ready."
 kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.29.2/manifests/calico.yaml
