91be923b33dcffcc591dffb231b938fe2b4a701f
Göran Pöhner <goeran.poehner@t-systems.com>
2025-05-21 09:27:43 +0200

    Updated to latest versions of kind (0.29.0) and calico (3.30.0)

--- kind/prepare-kind.sh
+++ kind/prepare-kind.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo ">>>>> Bootstrapping Kind"
-kind create cluster --name $1 --config kind-default.yaml --image kindest/node:v1.33.1@sha256:8d866994839cd096b3590681c55a6fa4a071fdaf33be7b9660e5697d2ed13002
+kind create cluster --name $1 --config kind-default.yaml --image kindest/node:v1.33.1@sha256:050072256b9a903bd914c0b2866828150cb229cea0efe5892e2b644d5dd3b34f
 kind export kubeconfig --name $1
 echo  "<<<<< Kind ready."
-kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.29.2/manifests/calico.yaml
+kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.30.0/manifests/calico.yaml
