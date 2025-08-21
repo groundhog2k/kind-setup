2be3da828319cd0f3c55e27560c3d0a840915746
Goeran Poehner <goeran.poehner@gmail.com>
2025-08-21 11:49:35 +0200

    Updated kubeseal and calico

--- kind/prepare-kind.sh
+++ kind/prepare-kind.sh
@@ -3,4 +3,4 @@ echo ">>>>> Bootstrapping Kind"
 kind create cluster --name $1 --config kind-default.yaml --image kindest/node:v1.33.1@sha256:050072256b9a903bd914c0b2866828150cb229cea0efe5892e2b644d5dd3b34f
 kind export kubeconfig --name $1
 echo  "<<<<< Kind ready."
-kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.30.0/manifests/calico.yaml
+kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.30.1/manifests/calico.yaml
