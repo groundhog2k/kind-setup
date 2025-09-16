881cdcbf1e1b78e264d14eccc8c84d4a68c5a39f
Göran Pöhner <goeran.poehner@t-systems.com>
2025-09-16 09:07:24 +0200

    Updated kubeseal

--- cluster-system/sealed-secrets/install.sh
+++ cluster-system/sealed-secrets/install.sh
@@ -2,7 +2,7 @@
 helm repo add sealed-secrets https://bitnami-labs.github.io/sealed-secrets
 helm repo update
 helm upgrade -i sealed-secrets -n cluster-system sealed-secrets/sealed-secrets -f sealed-secrets-values.yaml
-kseal_version="0.32.1"
+kseal_version="0.32.2"
 wget https://github.com/bitnami-labs/sealed-secrets/releases/download/v${kseal_version}/kubeseal-${kseal_version}-linux-amd64.tar.gz
 sudo tar Cxvzf /usr/local/bin kubeseal-${kseal_version}-linux-amd64.tar.gz kubeseal
 sudo chmod 755 /usr/local/bin/kubeseal
