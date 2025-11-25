05372cee7a504cbbf4a919eac776e183d129c0ee
Göran Pöhner <goeran.poehner@gmail.com>
2025-11-25 22:02:26 +0100

    Envoy setup

--- cluster-system/gateway-api/install.sh
+++ cluster-system/gateway-api/install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo ">>>>> Installing Gateway API"
 echo "Installing Gateway API CRDs"
-helm repo add istio https://istio-release.storage.googleapis.com/charts --force-update
-helm upgrade istio-base istio/base -n cluster-system -f istio-values.yaml --wait --wait-for-jobs -i
-helm upgrade istiod istio/istiod -n cluster-system -f istio-values.yaml --wait --wait-for-jobs -i
+helm install envoy-gateway oci://docker.io/envoyproxy/gateway-helm --version 1.6.0 -n cluster-system
+kubectl apply -f gatewayclass.yaml
 kubectl apply -n cluster-system -f gateway.yaml
+kubectl apply -n cluster-system -f route.yaml
 echo "<<<<< Gateway API ready."
