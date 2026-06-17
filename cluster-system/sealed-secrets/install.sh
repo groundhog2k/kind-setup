c888aaef6d8a7facab0ed1aea2420276e8ae80e1
Goeran Poehner <goeran.poehner@gmail.com>
2026-06-17 08:48:07 +0200

    Updated sealed-secrets installation

--- cluster-system/sealed-secrets/install.sh
+++ cluster-system/sealed-secrets/install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-helm repo add sealed-secrets https://bitnami-labs.github.io/sealed-secrets
+helm repo add sealed-secrets https://bitnami.github.io/sealed-secrets
 helm repo update
 helm upgrade -i sealed-secrets -n cluster-system sealed-secrets/sealed-secrets -f sealed-secrets-values.yaml
 kseal_version="0.37.0"
-wget https://github.com/bitnami-labs/sealed-secrets/releases/download/v${kseal_version}/kubeseal-${kseal_version}-linux-amd64.tar.gz
+wget https://github.com/bitnami/sealed-secrets/releases/download/v${kseal_version}/kubeseal-${kseal_version}-linux-amd64.tar.gz
 sudo tar Cxvzf /usr/local/bin kubeseal-${kseal_version}-linux-amd64.tar.gz kubeseal
 sudo chmod 755 /usr/local/bin/kubeseal
 rm kubeseal-${kseal_version}-linux-amd64.tar.gz
