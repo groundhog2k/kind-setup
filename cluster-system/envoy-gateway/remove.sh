086cf7dca379600c564efa117b481def63cc78d5
Göran Pöhner <goeran.poehner@telekom.de>
2026-06-23 15:37:32 +0200

    Fixed configs

--- cluster-system/envoy-gateway/remove.sh
+++ cluster-system/envoy-gateway/remove.sh
@@ -2,6 +2,7 @@
 echo ">>>>> Installing Envoy Gateway"
 kubectl delete -f gateway.yaml -n cluster-system
 kubectl delete -f gateway-class.yaml -n cluster-system
+kubectl delete -f envoyproxy.yaml -n cluster-system
 helm uninstall envoy-gateway -n cluster-system --wait
 helm template eg oci://docker.io/envoyproxy/gateway-crds-helm --version v1.8.1 --set crds.gatewayAPI.enabled=false --set crds.envoyGateway.enabled=true | kubectl delete -f -
 echo "<<<<< Envoy Gateway ready."
