db125439511df82f08ac337878c4ee785413feb0
Göran Pöhner <goeran.poehner@gmail.com>
2026-06-24 18:45:41 +0200

    Updated deployment

--- cluster-system/cert-manager/install.sh
+++ cluster-system/cert-manager/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo ">>>>> Installing Kubernetes Gateway API CRDs"
-kubectl apply --server-side -f https://github.com/kubernetes-sigs/gateway-api/releases/download/v1.5.0/standard-install.yaml
+kubectl apply --server-side -f https://github.com/kubernetes-sigs/gateway-api/releases/download/v1.5.1/standard-install.yaml
 echo "<<<<< Kubernetes Gateway API CRDs ready."
 echo ">>>>> Installing Jetstack cert-manager"
 helm repo add jetstack https://charts.jetstack.io --force-update
--- cluster-system/envoy-gateway/install.sh
+++ cluster-system/envoy-gateway/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo ">>>>> Installing Envoy Gateway"
 helm template eg oci://docker.io/envoyproxy/gateway-crds-helm --version v1.8.0 --set crds.gatewayAPI.enabled=false --set crds.envoyGateway.enabled=true | kubectl apply --server-side -f -
-helm install envoy-gateway oci://docker.io/envoyproxy/gateway-helm --version v1.8.0 -n cluster-system -f gateway-values.yaml --skip-crds --wait --wait-for-jobs
+helm install envoy-gateway oci://docker.io/envoyproxy/gateway-helm --version v1.8.0 -n cluster-system --skip-crds --wait --wait-for-jobs
 kubectl apply -f gateway-class.yaml -n cluster-system
 kubectl apply -f envoyproxy.yaml -n cluster-system
 kubectl apply -f gateway.yaml -n cluster-system
